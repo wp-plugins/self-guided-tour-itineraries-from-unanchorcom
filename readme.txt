3f972c705e400a2942692dace264f9eecec11669
unanchor <unanchor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-03 21:44:02 +0000

    Initial commit.
    git-svn-id: https://plugins.svn.wordpress.org/self-guided-tour-itineraries-from-unanchorcom/trunk@418976 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,71 @@
+=== Self Guided Tour Itineraries from Unanchor.com (Widget) ===
+
+Contributors: Mohammad
+Donate link: http://www.unanchor.com/
+Tags: widget, travel, self guided tour itineraries, travel planning, sidebar, cache, travel guide, travel itinerary, trip planning
+Requires at least: 2.0.0
+Tested up to: 3.2.1
+Stable tag: trunk
+
+Customizable sidebar widget: displays user's self-guided tour itineraries from Unanchor.com or just the latest self-guided tour itineraries.
+
+
+== Description ==
+
+= Recent posts with thumbnails! =
+
+This Wordpress plugin installs a **new sidebar widget** that can display a self-guided tour itineraries from the [Unanchor.com](http://www.unanchor.com/ "Self-Guided Tour Itineraries from Unanchor.com") website. 
+
+This plugin provides a widget control panel to control the number of itineraries displayed, the username of the unanchor writer, as well as the title of the widget itself.
+
+Itinerary details are retreived from the Unanchor API and cached in wordpress to maximize speed - the cache is renewed every 24 hours.
+
+You can use the provided additional stylesheet, or customize your own.
+
+
+= Active support =
+
+This is an official **Unanchor** Wordpress plugin - this means you can contact the team at wp@unanchor.com and we will try to help you resolve any issues with the plugin.
+
+
+== Installation ==
+
+1. Unzip self-guided-tour-itineraries-from-unanchorcom.zip
+1. Upload the `self-guided-tour-itineraries-from-unanchorcom` directory and all its contents into the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Use the widgets admin page to add the widget to one of your sidebars and configure it
+
+
+== Frequently Asked Questions ==
+
+= Where should I ask questions? =
+
+Any questions and comments regarding this plugin can be sent to wp@unanchor.com
+
+
+= I made some changes but they do not show up on the site... =
+
+Itineraries are cached for 24 hours to increase performance. If you want to make changes appear right away, go into the widget control panel and click **Save Changes** - this will clear the cache.
+
+
+
+== Screenshots ==
+
+1. An example of the sidebar widget in action
+
+2. Another example of the recent post list rendering
+
+3. The widget control pannel in Wordpress 2.3
+
+
+
+== Widget control pannel ==
+
+The widget has its own control pannel for setting up your unanchor username, the number of itineraries to display, and what the title of your widget should be. You can administer it from the widgets admin page.
+
+
+
+== Changelog ==
+
+= 1.1 =
+* Initial release
---
+++ self-guided-tour-itineraries-from-unanchorcom.php
@@ -0,0 +1,202 @@
+<?php
+/*
+Plugin Name: Self Guided Tour Itineraries from Unanchor.com 
+Plugin URI: http://blog.unanchor.com/2011/08/unanchor-wordpress-plugin-for-writers/
+Description: Displays a list of Self-Guided Tour Itineraries from Unanchor.com. If an unanchor username is provided, it will only return itineraries written by that user.
+Author: Unanchor.com
+Version: 1.1
+Author URI: http://www.unanchor.com/
+*/
+
+
+    /**
+      * When the plugin is activated, clear the cache, call the unanchor API and cache the results
+      */
+    function SGTI_activate() {
+
+        // Create default settings
+        if( !get_option("SGTI_options") ) {
+
+            $defaults = array();
+            $defaults['title'] = "Unanchor Tour Itineraries";
+            $defaults['header_element'] = "h2";
+            
+            update_option("SGTI_options", $defaults );
+        }
+         
+        if( !get_option("SGTI_widget_options") ) {
+
+            $defaults = array();
+            $defaults['title'] = "Unanchor Tour Itineraries";
+            $defaults['num_display'] = 5;
+            $defaults['unanchor_username'] = '';
+    
+            update_option("SGTI_widget_options", $defaults );
+
+        }
+
+        // Clear cache
+        delete_option('SGTI_cache');
+
+        // Regenerate cache of unanchor API call
+        $itineraries = SGTI_get_itineraries();
+
+    }
+    
+    
+    function SGTI_load_includes() {
+        
+        echo '<link rel="stylesheet" type="text/css" href="'.get_option('siteurl').'/wp-content/plugins/self-guided-tour-itineraries-from-unanchorcom/main.css" />';
+    
+    }
+
+
+
+    /**
+      Checks the cache data in the wordpress options table
+      if the data exists, and it has not expired, it is returned
+      if the data does not exist, or it has expired, false is returned
+    **/
+    function SGTI_get_from_cache() {
+        $data = false;
+
+        // check if we have a cached version
+        if($cache_info = get_option('SGTI_cache')){
+
+            // make sure the cache has not expired
+//            var_dump($cache_info);exit;
+            if(!is_numeric($cached_info[1]) || $cached_info[1] < (time()-86400)) {
+                $data = false;
+            } else {
+                $data = $cached_info[0];
+            }
+
+        }
+
+        return $data;
+    }
+
+
+    function SGTI_get_itineraries() {
+
+        // check if we have a cached version, if so, just use that
+        if($cache = SGTI_get_from_cache()){
+            return json_decode($cache);
+        }
+
+        // if we have curl
+        if (function_exists('curl_init')) {
+            $url = get_permalink($post_id);
+
+            // create a new cURL resource
+            $ch = curl_init();
+
+            // set URL and other appropriate options
+            $widget_options = get_option('SGTI_widget_options');
+            if(!empty( $widget_options['unanchor_username'] )){
+                curl_setopt($ch, CURLOPT_URL, 'http://api.unanchor.com/1.0/itinerary/list.json?username=' . urlencode($widget_options['unanchor_username']));
+            } else {
+                curl_setopt($ch, CURLOPT_URL, 'http://api.unanchor.com/1.0/itinerary/list.json');
+            }
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+            $api_json_response = curl_exec($ch);
+
+            curl_close($ch);
+            
+            // update the cache
+            update_option('SGTI_cache', array($api_json_response, time()));
+
+            return json_decode($api_json_response);
+        } else {
+            echo '<strong>Your server does not have PHP cURL support. Please contact your webhost and ask them to enable cURL for PHP.</strong>';
+            exit;
+        }
+
+    }
+
+
+    /********** Widget functions **********/
+    function SGTI_widget( $args ) {
+
+        $options = get_option('SGTI_widget_options');
+    
+        $json_object = SGTI_get_itineraries();
+                    
+        echo $before_widget;
+        echo "<div id=\"unanchor-itineraries\">\n";
+        echo "<h2>".$options['title']."</h2>\n";
+        echo "<ul>\n";
+
+        $i = 0;
+        foreach( $json_object->itineraries as $itinerary  ) {
+            if($i < $options['num_display']) {
+                echo '<li><a href="http://www.unanchor.com/itinerary/'.$itinerary->slug.'.html" title="'.attribute_escape($itinerary->title).'"'.
+                        '><img src="http://www.unanchor.com/uploads/itinerary/images/thumbnail/size2/'.$itinerary->image_filepath.'" width="35" height="35"></a> '.
+                        '<a href="http://www.unanchor.com/itinerary/'.$itinerary->slug.'.html" title="'.attribute_escape($itinerary->title).'">'.
+                        $itinerary->title.'</a></li><div style="clear: both;"></div>';
+            } else {
+                break;
+            }
+            $i++;
+        }
+                
+        echo '</ul></div><div style="display: none;">powered by <a href="http://www.unanchor.com" title="Self Guided Tour Itineraries">Unanchor.com</a></div>';
+                
+        echo $after_widget;
+                
+    }
+
+
+    function SGTI_widget_control() {
+    
+        $options = $new_options = get_option('SGTI_widget_options');
+    
+        if ( $_POST['SGTI_submit'] ) {
+            $options['title'] = strip_tags( stripslashes( $_POST['SGTI_widget_title'] ) );
+            $options['num_display'] = strip_tags( stripslashes( $_POST['SGTI_num_display'] ) );
+            $options['unanchor_username'] = strip_tags( stripslashes( $_POST['SGTI_username'] ) );
+
+            update_option('SGTI_widget_options', $options);
+
+            // Clear cache
+            delete_option('SGTI_cache');
+        }
+        
+        $title = attribute_escape($options['title']);
+        
+        $num_display = attribute_escape( $options['num_display'] );
+        $unanchor_username = attribute_escape( $options['unanchor_username'] );
+        
+    ?>
+         
+        <p><label for="pages-title"><?php _e('Title:'); ?></label> <input class="widefat" id="SGTI_widget_title" name="SGTI_widget_title" type="text" value="<?php echo $title; ?>" /></p>
+        <p><label for="pages-title"><?php _e('Unanchor Username:'); ?></label> <input class="widefat" id="SGTI_username" name="SGTI_username" type="text" value="<?php echo $unanchor_username; ?>" /></p>
+        <p><label for="pages-title"><?php _e('No. of Itineraries to Show:'); ?></label> <input class="widefat" id="SGTI_num_display" name="SGTI_num_display" type="text" value="<?php echo $num_display; ?>" /></p>
+        
+        <input type="hidden" id="SGTI_submit" name="SGTI_submit" value="1" />
+    <?php
+    }
+    
+    function SGTI_load_widget() {
+    
+        $widget_ops = array('classname' => 'unanchor_self_guided_tour_itineraries', 'description' => __( "Include Unanchor Itineraries in your sidebar") );
+        wp_register_sidebar_widget('unanchor_self_guided_tour_itineraries', __('Unanchor Itineraries'), 'SGTI_widget', $widget_ops);
+        wp_register_widget_control('unanchor_self_guided_tour_itineraries', __('Unanchor Itinerary display options'), 'SGTI_widget_control' );
+        
+    }
+    
+
+
+
+
+    
+/******* Hooks and Filters ************/
+register_activation_hook( __FILE__, 'SGTI_activate' );
+
+add_action('widgets_init', 'SGTI_load_widget');
+add_action('wp_head', "SGTI_load_includes");
+
+?>
